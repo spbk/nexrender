6d690d9b256083941611bf78cf743adf469bbcbb
Joey Stevens <joey.stevens00@gmail.com>
2022-10-04 15:15:29 -0400

    debug

--- packages/nexrender-action-webhook/index.js
+++ packages/nexrender-action-webhook/index.js
@@ -3,6 +3,7 @@ const tls = require('node:tls');
 const https = require('https');
 const crypto = require('node:crypto');
 const url      = require('url');
+const { checkServerIdentity } = require('node:tls');
 
 function sha256(s) {
     return crypto.createHash('sha256').update(s).digest('base64');
@@ -24,6 +25,59 @@ module.exports = (job, settings, { input, params, ...options }, type) => {
 
         const callback_url = url.parse(params.callback);
         const postData = JSON.stringify(job);
+        const checkServerIdentity = function(host, cert) {
+            console.log("Custom checkServerIdentity cb");
+            if (process.env.ACTION_WEBHOOK_SKIP_SSL_VALIDATION) {
+                return;
+            }
+            // Make sure the certificate is issued to the host we are connected to
+            const err = tls.checkServerIdentity(host, cert);
+            if (err) {
+              console.log("tls.checkServerIdentity err: ", err);
+              return err;
+            }
+        
+            // Pin the public key, similar to HPKP pin-sha256 pinning
+            if (process.env.PIN_PUB_KEY_256) {
+                const pubkey256 = process.env.PIN_PUB_KEY_256;
+                if (sha256(cert.pubkey) !== pubkey256) {
+                  const msg = 'Certificate verification error: ' +
+                    `The public key of '${cert.subject.CN}' ` +
+                    'does not match our pinned fingerprint';
+                  return new Error(msg);
+                }
+            }
+
+        
+            // Pin the exact certificate, rather than the pub key
+            if (process.env.PIN_CERT_256) {
+                const cert256 = process.env.PIN_CERT_256;
+              if (cert.fingerprint256 !== cert256) {
+                const msg = 'Certificate verification error: ' +
+                  `The certificate of '${cert.subject.CN}' ` +
+                  'does not match our pinned fingerprint';
+                return new Error(msg);
+              }
+            }
+
+        
+            // This loop is informational only.
+            // Print the certificate and public key fingerprints of all certs in the
+            // chain. Its common to pin the public key of the issuer on the public
+            // internet, while pinning the public key of the service in sensitive
+            // environments.
+            do {
+              console.log('Subject Common Name:', cert.subject.CN);
+              console.log('  Certificate SHA256 fingerprint:', cert.fingerprint256);
+        
+              hash = crypto.createHash('sha256');
+              console.log('  Public key ping-sha256:', sha256(cert.pubkey));
+        
+              lastprint256 = cert.fingerprint256;
+              cert = cert.issuerCertificate;
+            } while (cert.fingerprint256 !== lastprint256);
+        
+          };
         const http_options = {
             hostname: callback_url.host,
             path: callback_url.path,
@@ -34,63 +88,12 @@ module.exports = (job, settings, { input, params, ...options }, type) => {
                 'Content-Length': Buffer.byteLength(postData)
             },
             rejectUnauthorized: process.env.ACTION_WEBHOOK_SKIP_SSL_VALIDATION ? false : true,
-            checkServerIdentity: function(host, cert) {
-                console.log("Custom checkServerIdentity cb");
-                if (process.env.ACTION_WEBHOOK_SKIP_SSL_VALIDATION) {
-                    return;
-                }
-                // Make sure the certificate is issued to the host we are connected to
-                const err = tls.checkServerIdentity(host, cert);
-                if (err) {
-                  console.log("tls.checkServerIdentity err: ", err);
-                  return err;
-                }
-            
-                // Pin the public key, similar to HPKP pin-sha256 pinning
-                if (process.env.PIN_PUB_KEY_256) {
-                    const pubkey256 = process.env.PIN_PUB_KEY_256;
-                    if (sha256(cert.pubkey) !== pubkey256) {
-                      const msg = 'Certificate verification error: ' +
-                        `The public key of '${cert.subject.CN}' ` +
-                        'does not match our pinned fingerprint';
-                      return new Error(msg);
-                    }
-                }
-
-            
-                // Pin the exact certificate, rather than the pub key
-                if (process.env.PIN_CERT_256) {
-                    const cert256 = process.env.PIN_CERT_256;
-                  if (cert.fingerprint256 !== cert256) {
-                    const msg = 'Certificate verification error: ' +
-                      `The certificate of '${cert.subject.CN}' ` +
-                      'does not match our pinned fingerprint';
-                    return new Error(msg);
-                  }
-                }
-
-            
-                // This loop is informational only.
-                // Print the certificate and public key fingerprints of all certs in the
-                // chain. Its common to pin the public key of the issuer on the public
-                // internet, while pinning the public key of the service in sensitive
-                // environments.
-                do {
-                  console.log('Subject Common Name:', cert.subject.CN);
-                  console.log('  Certificate SHA256 fingerprint:', cert.fingerprint256);
-            
-                  hash = crypto.createHash('sha256');
-                  console.log('  Public key ping-sha256:', sha256(cert.pubkey));
-            
-                  lastprint256 = cert.fingerprint256;
-                  cert = cert.issuerCertificate;
-                } while (cert.fingerprint256 !== lastprint256);
-            
-              },
+            checkServerIdentity: checkServerIdentity,
+            agent: false,
         };
   
-        http_options.agent = new https.Agent(http_options);
-        console.log(http_options);
+        //http_options.agent = new https.Agent(http_options);
+        //console.log(http_options);
         const req = https.request(http_options, (res) => {
           console.log('All OK. Server matched our pinned cert or public key');
           console.log('statusCode:', res.statusCode);
