4c8de22ebb08e7c394ab97e890599073656dcf27
Kevin Coleman <kcoleman@spaceback.me>
2023-07-13 14:33:45 -0400

    Debugging tracer.

--- packages/nexrender-core/src/helpers/tracer.js
+++ packages/nexrender-core/src/helpers/tracer.js
@@ -10,8 +10,13 @@ class NoopTracer {
     trace(_name, cb) { return cb(new NoopSpan()) }
 }
 
+console.log("process.env.ENABLE_DATADOG_APM: " + process.env.ENABLE_DATADOG_APM)
+console.log("process.env.DD_TRACE_AGENT_URL: " + process.env.DD_TRACE_AGENT_URL)
+
 if(process.env.ENABLE_DATADOG_APM) {
+    console.log("Datadog APM enabling...");
     tracer = require('dd-trace').init();
+    console.log(JSON.stringify(tracer.options))
 } else {
     // define noop tracer if datadog is not enabled
     tracer =  new NoopTracer()
